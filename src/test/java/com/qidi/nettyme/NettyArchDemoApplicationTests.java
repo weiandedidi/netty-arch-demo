605c0a613b2b353b11b2f1635e4517d29322b77b
weiandedidi <weiandedidi@163.com>
2024-11-04 14:31:39 +0800

    first commit

---
+++ src/test/java/com/qidi/nettyme/NettyArchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qidi.nettyme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyArchDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
